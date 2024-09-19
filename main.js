76c8ed3edc8965be3e7fd5314488125e1db600e0
Maxence <maxence.gvr@gmail.com>
2024-09-19 22:11:50 +0200

    pre-final ✨

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 'use strict';
 (function(){
-    console.log('App loaded [Made by Maxence (mobile.maxence-dev.fr)]');
+    const [$, $$] = [(s) => document.querySelector(s), (s) => document.querySelectorAll(s)];
+
+    document.addEventListener('DOMContentLoaded', function () {
+        console.log('App loaded [Made by Maxence (mobile.maxence-dev.fr)]');
+
+        window.scrollTo(0, 0);
+
+        document.addEventListener('scroll', function (evt) {
+            const percent = window.scrollY / (document.body.scrollHeight - window.innerHeight) * 100;
+
+            $('.scroll-indicator-percent').innerText = `${Math.round(percent)}%`;
+            $('.header-scroll-indicator').style = `--wp: ${Math.round(percent)}%`;
+        });
+
+        $('#toggle').addEventListener('change', function (evt) {
+            let checked = evt.target.checked;
+            $$('.card-container').forEach((card) => {
+                card.classList.toggle('sticky');
+                card.classList.toggle('relative');
+            });
+        });
+
+        $$('.header-title').forEach(btn => {
+            btn.addEventListener('click', () => window.scrollTo(0, 0));
+        })
+    });
 })();
